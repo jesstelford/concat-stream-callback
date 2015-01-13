03ada4939c33d5ce59fbe8ea353cedfc3ed63804
Jess Telford <jtelford@groupon.com>
2015-01-14 08:41:36 +1100

    Options tests

---
+++ test/options.test.js
@@ -0,0 +1,37 @@
+var assert = require('assert'),
+    Readable = require('stream').Readable,
+    concatStreamCallback = require('../');
+
+describe('Options', function() {
+
+  beforeEach('setup stream', function() {
+    this.stream = new Readable();
+    this.stream._read = (function() {
+      this.push('hello');
+      this.push(null);
+    }).bind(this.stream)
+  });
+
+  it('are optional', function(done) {
+
+    concatStreamCallback(this.stream, function(err, result) {
+      assert(err == null);
+      assert(result == 'hello');
+      done();
+    });
+
+  });
+
+  it('are passed through to concat-stream', function(done) {
+
+    var callCount = 0;
+
+    concatStreamCallback(this.stream, {encoding: 'buffer'}, function(err, result) {
+      assert(err == null);
+      assert(Buffer.isBuffer(result));
+      done();
+    });
+
+  });
+
+});
